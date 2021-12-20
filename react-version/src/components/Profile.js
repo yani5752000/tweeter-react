3d6905149efed35c20ac9a996ff7400b6f8b3940
Farid <yani5752000@yahoo.com>
2021-12-20 17:31:10 -0500

    added html to react part

---
+++ react-version/src/components/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react'; //optional
+
+  function Profile(){
+    return (
+      <aside>
+        <div className="profile">
+          <img className="profile__image" src="./profile-hex.png"></img>
+        </div>
+        <br />
+        <div className="profile__name">
+          <h2><span className="profile--bold">Amy</span> Mansell</h2>
+        </div>
+      </aside>
+    );
+  }
+
+  export default Profile;
---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,15 @@
+import React from 'react'; //optional
+
+function TweetForm(){
+  return (
+    <section className="newtweet">
+      <form method="post" action="/tweets" className="newtweet__form">
+        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+        <input type="submit" value="Tweet" className="form__input"></input>
+        <span className="form__counter">140</span>
+      </form>
+    </section>
+  );
+}
+
+export default TweetForm;
