fcc8a1dca754fddf2adfa03b6cda26cb9975ae3b
Farid <yani5752000@yahoo.com>
2021-12-22 15:29:23 -0500

    form was prevented from default

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -2,9 +2,12 @@ import React from 'react'; //optional
 
 function TweetForm(){
   const placeholder = "What are you humming about?";
+  const doStuff = (event) => {
+    event.preventDefault();
+  }
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form onSubmit={doStuff} method="post" action="/tweets" className="newtweet__form">
         <textarea className="form__textarea" name="text" placeholder={placeholder}></textarea>
         <input type="submit" value="Tweet" className="form__input"></input>
         <span className="form__counter">140</span>
